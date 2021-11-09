96a0d44cb3c426f6f7b357fe8591161e298b6289
JavLock <jlocks1641@gmail.com>
2021-11-09 07:21:40 +0000

    change service

--- system-apidata/src/main/java/com/github/javlock/system/apidata/systemd/data/ServicesDemoManual.java
+++ system-apidata/src/main/java/com/github/javlock/system/apidata/systemd/data/ServicesDemoManual.java
@@ -5,7 +5,7 @@ public class ServicesDemoManual {
 	public static final String UPDATERSERVICEDATA = "[Unit]\n" + "Description=JavLock-System-Updater \n"
 			+ "Wants=network.target\n" + "After=network.target\n" + "\n" + "[Service]\n" + "User=root\n"
 			+ "Group=root\n" + "\n" + "WorkingDirectory=/tmp/\n"
-			+ "ExecStart=bash -c \"java -jar /opt/javlock-system/system-updater/target/system-updater-*-git-*-jar-with-dependencies.jar\"\n"
-			+ "\n" + "Restart=always\n" + "\n" + "[Install]\n" + "WantedBy=multi-user.target\n" + "";
+			+ "ExecStart=bash -c \"java -jar /opt/javlock-system/exe-jars/system-updater.jar\"\n" + "\n"
+			+ "Restart=always\n" + "\n" + "[Install]\n" + "WantedBy=multi-user.target\n" + "";
 
 }
